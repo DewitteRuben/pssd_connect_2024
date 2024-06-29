8c1fc527755f8d787983ceb93b9651d1fa8d63c9
DewitteRuben <rubendewitte1998@gmail.com>
2024-06-29 23:13:22 +0200

    chore: change interface text

--- src/pages/Messages/Messages.tsx
+++ src/pages/Messages/Messages.tsx
@@ -8,18 +8,29 @@ import {
   MessageInput,
   Thread,
   useCreateChatClient,
+  Streami18n,
 } from "stream-chat-react";
 import "stream-chat-react/dist/css/v2/index.css";
 import { useStore } from "../../store/store";
 
+const channelListOverrides = {
+  "You have no channels currently": "You currently have no ongoing conversations",
+};
+
 const apiKey = "gcdbeuybxb5j";
+const i18nInstance = new Streami18n({
+  language: "en",
+  translationsForLanguage: {
+    ...channelListOverrides,
+  },
+});
 
 const Messages = () => {
   const { user: userStore } = useStore();
 
   if (!userStore.user?.uid) return <div>Loading...</div>;
 
-  const userId = userStore.user?.uid
+  const userId = userStore.user?.uid;
   const filters = { members: { $in: [userId] }, type: "messaging" };
   const options = { presence: true, state: true };
   const sort = { last_message_at: -1 };
@@ -33,7 +44,7 @@ const Messages = () => {
   if (!client) return <div>Loading...</div>;
 
   return (
-    <Chat client={client}>
+    <Chat client={client} i18nInstance={i18nInstance}>
       <ChannelList filters={filters} options={options} />
       <Channel>
         <Window>
