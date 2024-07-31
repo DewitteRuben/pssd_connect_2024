bbfb76de174cdb27f440dc1c83503a1ef33d39e9
DewitteRuben <rubendewitte1998@gmail.com>
2024-07-31 04:23:05 +0200

    build: vite pwa setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
+import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), react()],
+  plugins: [
+    nodePolyfills(),
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
 });
