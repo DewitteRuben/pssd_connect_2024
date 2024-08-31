6eb08e1f42b905bda839965a0b96ee7805ac8986
DewitteRuben <rubendewitte1998@gmail.com>
2024-08-31 17:51:34 +0200

    fix: login with admin database locally

--- Makefile
+++ Makefile
@@ -1,8 +1,16 @@
 up:
-	docker compose up
+	docker compose up -d
 
 build:
 	docker compose build
+
+build-frontend:
+	npm run build
+
+build-all: build-frontend build
+
+stop:
+	docker compose down
 
 down:
 	docker compose down && docker compose rm -f
--- src/backend/src/database/mongo.ts
+++ src/backend/src/database/mongo.ts
@@ -1,7 +1,12 @@
 import mongoose from "mongoose";
 
-const { DATABASE_URL, MONGO_USERNAME, MONGO_PASSWORD, MONGO_INITDB_DATABASE } =
-  process.env;
+const {
+  DATABASE_URL,
+  MONGO_USERNAME,
+  MONGO_PASSWORD,
+  MONGO_INITDB_DATABASE,
+  ENVIRONMENT,
+} = process.env;
 
 export class MongoDB {
   private static mongoose: typeof mongoose;
@@ -13,7 +18,7 @@ export class MongoDB {
     this.mongoose = await mongoose.connect(DATABASE_URL, {
       user: MONGO_USERNAME,
       dbName: MONGO_INITDB_DATABASE,
-      authSource: MONGO_INITDB_DATABASE,
+      authSource: ENVIRONMENT === "DEV" ? "admin" : MONGO_INITDB_DATABASE,
       pass: MONGO_PASSWORD,
     });
   }
