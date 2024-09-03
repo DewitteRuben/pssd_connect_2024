f42e846cdfff9be51e34156fe8387eba0270cc7f
DewitteRuben <rubendewitte1998@gmail.com>
2024-09-03 11:43:51 +0200

    feat: on notif click open app

--- public/sw.js
+++ public/sw.js
@@ -34,3 +34,20 @@ onBackgroundMessage(messaging, ({ notification }) => {
 
     self.registration.showNotification(notificationTitle, notificationOptions)
 })
+
+
+self.addEventListener('notificationclick', event => {
+    const rootUrl = new URL('/', location).href;
+    event.notification.close();
+    // Enumerate windows, and call window.focus(), or open a new one.
+    event.waitUntil(
+        clients.matchAll().then(matchedClients => {
+            for (let client of matchedClients) {
+                if (client.url === rootUrl) {
+                    return client.focus();
+                }
+            }
+            return clients.openWindow("/");
+        })
+    );
+});
