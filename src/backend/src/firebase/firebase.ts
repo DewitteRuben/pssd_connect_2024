1752f658cd76ac181ccbe1d91e556ab0049a05dc
DewitteRuben <rubendewitte1998@gmail.com>
2024-09-19 11:27:29 +0200

    chore: temporary new credentials

--- src/backend/src/firebase/firebase.ts
+++ src/backend/src/firebase/firebase.ts
@@ -4,7 +4,6 @@ import serviceAccountKey from "../serviceAccountKey.json" assert { type: "json" 
 
 const FirebaseApp = admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey as ServiceAccount),
-  databaseURL: "https://pssd-app-default-rtdb.firebaseio.com",
 });
 
 export default FirebaseApp;
